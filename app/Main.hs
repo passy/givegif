0fbb5798115444c3d79f716b7e0459144d45d46c
Pascal Hartig <phartig@rdrei.net>
2016-04-13 21:38:30 +0100

    Exploit the functor instance of (,)

--- app/Main.hs
+++ app/Main.hs
@@ -102,8 +102,7 @@ main = do
                                     & join
 
       let dupeUrl = fstUrl & _Right %~ second show . dupe
-      let doReq (a, uri) = Wreq.get uri >>= (\resp' -> return (a, resp'))
-      resp' <- sequence $ doReq <$> dupeUrl
+      resp' <- sequence $ (sequence . fmap Wreq.get) <$> dupeUrl
 
       case resp' of
         Right r -> uncurry (printGif opts) r
